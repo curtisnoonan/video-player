96afa751a22a437ba2c4b376ad53e87643763ef5
Curtis Noonan <curtis.noonan@gmail.com>
2022-06-08 00:03:04 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+// Play & Pause ----------------------------------- //
+
+
+// On video end, show play button icon
+
+// Progress Bar ---------------------------------- //
+
+// Format current time, duration
+
+// Update progress bar as video plays
+
+
+// Click to seek within the video
+
+
+// Volume Controls --------------------------- //
+
+
+// Mute
+
+
+// Volume Bar
+
+// Change Playback Speed -------------------- //
+
+
+// Fullscreen ------------------------------- //
+
+/* View in fullscreen */
+
+
+/* Close fullscreen */
+
+
+// Toggle fullscreen
+
+// Event Listeners
